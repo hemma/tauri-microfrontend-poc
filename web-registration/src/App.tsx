94ece9c265017f8cf8b56743d31bb349d1c7981e
Johan Bothin <johan.bothin@gmail.com>
2022-07-14 22:00:54 +0200

    Init

---
+++ web-registration/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Center} from "@chakra-ui/react";
+
+function App() {
+
+  return (
+      <Center bg='tomato' h='100px' color='white'>
+        <h3>Registration</h3>
+      </Center>
+  )
+}
+
+export default App
